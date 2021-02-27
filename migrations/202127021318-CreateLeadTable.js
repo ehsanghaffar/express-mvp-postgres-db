fbcc3a709013b52265f67389a9da4d77b781875e
Ehsanghaffarii <ghafari.5000@gmail.com>
2021-02-27 13:37:01 +0330

    inital sequelize & define models and migrations

---
+++ migrations/202127021318-CreateLeadTable.js
@@ -0,0 +1,28 @@
+'use strict';
+module.exports = {
+  up: (queryInterface, Sequelize) => {
+    return queryInterface.createTable('Leads', {
+      id: {
+        allowNull: false,
+        primaryKey: true,
+        type: Sequelize.UUID,
+        defaultValue: Sequelize.UUIDV4,
+      },
+      createAt: {
+        allowNull: false,
+        type: Sequelize.DATE
+      },
+      updateAt: {
+        allowNull: false,
+        type: Sequelize.DATE
+      },
+      email: {
+        allowNull: false,
+        type: Sequelize.STRING
+      },
+    });
+  },
+  down: (queryInterface, Sequelize) => {
+    return queryInterface.dropTable('Leads');
+  }
+};
