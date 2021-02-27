8520075edeaef8536594b2755a35bf1c477affea
Ehsanghaffarii <ghafari.5000@gmail.com>
2021-02-27 11:39:15 +0330

    landing-page post route to submit email

--- controllers/landing.js
+++ controllers/landing.js
@@ -3,3 +3,8 @@
 exports.get_landing =  function(req, res, next) {
   res.render('landing', { title: 'Express' });
 };
+
+exports.submit_lead =  function(req, res, next) {
+  console.log("lead Email:", req.body.lead_email);
+  res.redirect('/');
+};
